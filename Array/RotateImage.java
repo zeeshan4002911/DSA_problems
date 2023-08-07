80c739031c9eb27da2e5c019c89661d09bd9fd22
AAA4002911 <zeeshan4002911@gmail.com>
2023-08-07 23:34:40 +0530

    soln added

---
+++ Array/RotateImage.java
@@ -0,0 +1,89 @@
+
+/**
+ * You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+ 
+ You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+ 
+ 
+ 
+ Example 1:
+ 
+ Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ Output: [[7,4,1],[8,5,2],[9,6,3]]
+ 
+ Example 2:
+ 
+ Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+ 
+
+Constraints:
+
+n == matrix.length == matrix[i].length
+1 <= n <= 20
+-1000 <= matrix[i][j] <= 1000
+
+
+*/
+import java.io.*;
+
+class RotateImage {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split("\\s+");
+            int r = Integer.parseInt(str[0]);
+            int c = Integer.parseInt(str[1]);
+            int matrix[][] = new int[r][c];
+
+            for (int i = 0; i < r; i++) {
+                int k = 0;
+                str = read.readLine().trim().split("\\s+");
+                for (int j = 0; j < c; j++) {
+                    matrix[i][j] = Integer.parseInt(str[k]);
+                    k++;
+                }
+            }
+            new Solution().rotate(matrix);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < r; i++) {
+                for (int j = 0; j < c; j++) {
+                    sb.append(matrix[i][j] + " ");
+                }
+                sb.append("\n");
+            }
+            System.out.print(sb);
+        }
+    }
+}
+
+class Solution {
+    public void rotate(int[][] matrix) {
+
+        /*
+         * For clockwise rotate, transpose then horizontal swaps
+         * and for Anticlockwise rotate, transpose and vertical swap
+         */
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < m; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
