129a6b198f5c3b59fa2b896f245e9d17ee606e51
AAA4002911 <zeeshan4002911@gmail.com>
2023-08-09 08:41:10 +0530

    soln added

---
+++ Array/ArithmeticSubArr.java
@@ -0,0 +1,105 @@
+
+/**
+ * A sequence of numbers is called arithmetic if it consists of at least two
+ * elements, and the difference between every two consecutive elements is the
+ * same. More formally, a sequence s is arithmetic if and only if s[i+1] - s[i]
+ * == s[1] - s[0] for all valid i.
+ * 
+ * For example, these are arithmetic sequences:
+ * 
+ * 1, 3, 5, 7, 9
+ * 7, 7, 7, 7
+ * 3, -1, -5, -9
+ * 
+ * The following sequence is not arithmetic:
+ * 
+ * 1, 1, 2, 5, 7
+ * 
+ * You are given an array of n integers, nums, and two arrays of m integers
+ * each, l and r, representing the m range queries, where the ith query is the
+ * range [l[i], r[i]]. All the arrays are 0-indexed.
+ * 
+ * Return a list of boolean elements answer, where answer[i] is true if the
+ * subarray nums[l[i]], nums[l[i]+1], ... , nums[r[i]] can be rearranged to form
+ * an arithmetic sequence, and false otherwise.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [4,6,5,9,3,7], l = [0,0,2], r = [2,3,5]
+ * Output: [true,false,true]
+ * Explanation:
+ * In the 0th query, the subarray is [4,6,5]. This can be rearranged as [6,5,4],
+ * which is an arithmetic sequence.
+ * In the 1st query, the subarray is [4,6,5,9]. This cannot be rearranged as an
+ * arithmetic sequence.
+ * In the 2nd query, the subarray is [5,9,3,7]. This can be rearranged as
+ * [3,5,7,9], which is an arithmetic sequence.
+ * 
+ * Example 2:
+ * 
+ * Input: nums = [-12,-9,-3,-12,-6,15,20,-25,-20,-15,-10], l = [0,1,6,4,8,7], r
+ * = [4,4,9,7,9,10]
+ * Output: [false,true,false,false,true,true]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * n == nums.length
+ * m == l.length
+ * m == r.length
+ * 2 <= n <= 500
+ * 1 <= m <= 500
+ * 0 <= l[i] < r[i] < n
+ * -105 <= nums[i] <= 105
+ * 
+ * 
+ */
+import java.io.*;
+import java.util.*;
+
+public class ArithmeticSubArr {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().trim().split(",");
+        String[] str_l = br.readLine().trim().split(",");
+        String[] str_h = br.readLine().trim().split(",");
+        br.close();
+        int[] arr = new int[str.length];
+        int[] low = new int[str_l.length];
+        int[] high = new int[str_h.length];
+        for (int i = 0; i < str.length; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        for (int i = 0; i < str_l.length; i++) {
+            low[i] = Integer.parseInt(str_l[i]);
+        }
+        for (int i = 0; i < str_h.length; i++) {
+            high[i] = Integer.parseInt(str_h[i]);
+        }
+        List<Boolean> result = checkArithmeticSubarrays(arr, low, high);
+        System.out.println(Arrays.toString(result.toArray()));
+    }
+
+    public static List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> result = new ArrayList<>();
+        for (int i = 0; i < l.length; i++) {
+            int[] arr = Arrays.copyOfRange(nums, l[i], r[i] + 1);
+            Arrays.sort(arr);
+            int common_diff = arr[1] - arr[0];
+            boolean addFlag = false;
+            for (int j = 2; j < arr.length; j++) {
+                if (arr[j] - arr[j - 1] != common_diff) {
+                    addFlag = true;
+                    result.add(false);
+                    break;
+                }
+            }
+            if (!addFlag)
+                result.add(true);
+        }
+        return result;
+    }
+}
