14954af55f0dfc5f8d071642f781a77e15909276
AAA4002911 <zeeshan4002911@gmail.com>
2023-08-10 08:12:14 +0530

    attemp1 added

---
+++ Array/CountSubArrProdLessThanK.java
@@ -0,0 +1,96 @@
+/**
+ * Given an array of positive numbers, the task is to find the number of possible contiguous subarrays having product less than a given number k.
+
+Example 1:
+
+Input : 
+n = 4, k = 10
+a[] = {1, 2, 3, 4}
+Output : 
+7
+Explanation:
+The contiguous subarrays are {1}, {2}, {3}, {4} 
+{1, 2}, {1, 2, 3} and {2, 3}, in all these subarrays
+product of elements is less than 10, count of
+such subarray is 7.
+{2,3,4} will not be a valid subarray, because 
+2*3*4=24 which is greater than 10.
+
+Example 2:
+
+Input:
+n = 7 , k = 100
+a[] = {1, 9, 2, 8, 6, 4, 3}
+Output:
+16
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function countSubArrayProductLessThanK() which takes the array a[], its size n and an integer k as inputs and returns the count of required subarrays.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1<=n<=106
+1<=k<=1015
+1<=a[i]<=105
+ */
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CountSubArrProdLessThanK {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(stt.nextToken());
+            long k = Long.parseLong(stt.nextToken());
+
+            long a[] = new long[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+
+            Solution obj = new Solution();
+            out.println(obj.countSubArrayProductLessThanK(a, n, k));
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+// User function Template for Java
+
+class Solution {
+
+    public long countSubArrayProductLessThanK(long a[], int n, long k) {
+        int j = 0;
+        int count = 0;
+        long product = 1;
+        for (int i = 0; i < n; i++) {
+            product *= a[i];
+            count++;
+            if (product > k) {
+                product /= a[j++];
+                while (product > k) {
+                    product /= a[i];
+                    i--;
+                }
+                count += (i - j);
+            }
+        }
+        // count -= (n - j);
+        // System.out.println("zz " + j);
+        return count;
+    }
+}
