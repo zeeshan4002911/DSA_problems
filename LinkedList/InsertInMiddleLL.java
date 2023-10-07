9ef40482a4c47ea2fd7051c405b0a2e75312618b
AAA4002911 <zeeshan4002911@gmail.com>
2023-10-07 23:49:08 +0530

    soln added

---
+++ LinkedList/InsertInMiddleLL.java
@@ -0,0 +1,115 @@
+/**
+ * Given a linked list of size N and a key. The task is to insert the key in the
+ * middle of the linked list.
+ * 
+ * Example 1:
+ * 
+ * Input:
+ * LinkedList = 1->2->4
+ * key = 3
+ * Output: 1 2 3 4
+ * Explanation: The new element is inserted
+ * after the current middle element in the
+ * linked list.
+ * 
+ * Example 2:
+ * 
+ * Input:
+ * LinkedList = 10->20->40->50
+ * key = 30
+ * Output: 10 20 30 40 50
+ * Explanation: The new element is inserted
+ * after the current middle element in the
+ * linked list and Hence, the output is
+ * 10 20 30 40 50.
+ * 
+ * 
+ * 
+ * Your Task:
+ * The task is to complete the function insertInMiddle() which takes head
+ * reference and element to be inserted as the arguments. The printing is done
+ * automatically by the driver code.
+ * 
+ * Expected Time Complexity : O(N)
+ * Expected Auxilliary Space : O(1)
+ * 
+ * Constraints:
+ * 1 <= N <= 104
+ */
+
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+
+            for (int i = 0; i < n - 1; i++) {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            int key = sc.nextInt();
+
+            // display(head);
+            Solution obj = new Solution();
+            obj.insertInMid(head, key);
+            display(head);
+            System.out.println();
+        }
+    }
+
+    public static void display(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// } Driver Code Ends
+
+/*
+ * Structure of node class is:
+ * class Node {
+ * int data;
+ * Node next;
+ * 
+ * public Node(int data){
+ * this.data = data;
+ * this.next = null;
+ * }
+ * }
+ */
+class Solution {
+
+    public Node insertInMid(Node head, int data) {
+        // Insert code here, return the head of modified linked list
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node newNode = new Node(data);
+        newNode.next = slow.next;
+        slow.next = newNode;
+        return head;
+    }
+}
