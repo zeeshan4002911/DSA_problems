9df14af7644969a512503dae66168f96398928da
AAA4002911 <zeeshan4002911@gmail.com>
2023-10-13 23:45:06 +0530

    optimised soln added

--- LinkedList/RemoveDuplicateLL.java
+++ LinkedList/RemoveDuplicateLL.java
@@ -109,15 +109,15 @@ class GfG {
         if (head == null)
             return null;
 
-        Node curr, past;
-        curr = past = head;
+        Node curr = head;
 
-        while (curr != null && curr.next != null) {
-            while (curr.next != null && curr.data == curr.next.data) {
-                curr.next = curr.next.next;
-            }
-            curr = curr.next;
-        }
+        while(curr.next != null){
+	        if (curr.data == curr.next.data){
+	            curr.next = curr.next.next;
+	        } else {
+	            curr = curr.next;
+	        }
+	    }
         return head;
     }
 }
