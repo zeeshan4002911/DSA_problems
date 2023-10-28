59ca2784e66f2199979a4489a6a3352c097cee93
AAA4002911 <zeeshan4002911@gmail.com>
2023-10-28 22:40:27 +0530

    soln to be added

--- Stack/SortStack.java
+++ Stack/SortStack.java
@@ -26,3 +26,37 @@
  * Constraints:
  * 1<=N<=100
  */
+
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.Stack;
+
+class SortedStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            Stack<Integer> s = new Stack<>();
+            int n = sc.nextInt();
+            while (n-- > 0)
+                s.push(sc.nextInt());
+            GfG g = new GfG();
+            Stack<Integer> a = g.sort(s);
+            while (!a.empty()) {
+                System.out.print(a.peek() + " ");
+                a.pop();
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+/* Complete the function below */
+class GfG {
+    public Stack<Integer> sort(Stack<Integer> s) {
+        // add code here.
+        
+        return s;
+    }
+}
